ff9b5aff5dfde308a510690ce50f80fd470171d3
= <luccashiroshi@gmail.com>
2022-05-04 15:11:51 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Arquivo SASS
+import './scss/index.scss'
+
+// Dependencias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
